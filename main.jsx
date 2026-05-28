d6e81e7c18d110d5cd4f14673f1304a4d4ecd288
damianwass <damian.wasserlauf@gmail.com>
2026-05-28 12:06:37 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,21 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+html {
+  -webkit-text-size-adjust: 100%;
+}
+
+body {
+  margin: 0;
+  min-width: 320px;
+  min-height: 100vh;
+  font-family: Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+}
+
+button,
+input,
+select,
+textarea {
+  font: inherit;
+}
